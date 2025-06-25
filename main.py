4d40f3b9a60f3fdd5b47324576f424deabe99bd1
Rise0x00 <rise.dhmn@gmail.com>
2025-06-26 01:07:50 +0300

    Add buttons locking

--- main.py
+++ main.py
@@ -22,7 +22,13 @@ class MyWindow(QMainWindow):
     def clear_output(self):
         self.ui.conOut.setText("")
 
+    def set_buttons_enable(self, state):
+        self.ui.Button_flash.setEnabled(state)
+        self.ui.Button_uploadfs.setEnabled(state)
+        self.ui.toolButton.setEnabled(state)
+
     def flash_button_clicked(self):
+        self.set_buttons_enable(False)
         self.clear_output()
         self.write_output("Начинаю прошивку...")
         # Получаем путь из поля ввода
@@ -31,13 +37,14 @@ class MyWindow(QMainWindow):
             self.write_output(f"Путь к проекту: {project_path}")
         else:
             self.write_output("Операция отменена. Путь к папке проекта не указан.")
-    
+        self.set_buttons_enable(True)
+
     def uploadfs_button_clicked(self):
-        """Обработчик нажатия кнопки Upload FS"""
+        self.set_buttons_enable(False)
         self.clear_output()
         self.write_output("Загружаю файловую систему...")
         self.ui.progressBar.setValue(0)
-        # Ваш код для загрузки файловой системы
+        self.set_buttons_enable(True)
     
     def browse_folder(self):
         """Обработчик кнопки выбора папки"""
@@ -46,7 +53,6 @@ class MyWindow(QMainWindow):
             self.ui.lineEdit.setText(folder_path)
             self.clear_output()
             self.write_output(f"Выбрана папка проекта: {folder_path}")
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
