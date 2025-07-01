7577d489a537299b9b6890791e6a2248ca07ef23
Rise0x00 <rise.dhmn@gmail.com>
2025-07-01 18:57:22 +0300

    Fix install scripts

--- install.cmd
+++ install.cmd
@@ -1,4 +1,3 @@
 pip3 install platformio
 pip3 install PyQt5
-pip3 install datetime
 python main.py
--- install.sh
+++ install.sh
@@ -3,5 +3,4 @@ sudo apt update
 sudo apt install -y python3-pip
 pip3 install platformio
 pip3 install PyQt5
-pip3 install datetime
 python3 main.py
